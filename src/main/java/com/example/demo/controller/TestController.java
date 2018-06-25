6d01f665dccd17e0bf10f225d9e0c5ea7d962698
lumeng <lumeng@111.com.cn>
2018-06-25 11:49:51 +0800

    dwdwd

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.demo.dto.Order;
 import com.example.demo.dto.User;
+import com.example.demo.mapper.master1.OrderMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private OrderMapper orderMapper;
+
 
     @RequestMapping("/getUser")
     public String getUser(User user){
         String json=JSONObject.toJSONString(user);
         return json;
+    }
+
+    @RequestMapping("/getOrderByOrderId")
+    public Order getOrderByorderId(String orderId){
+        return orderMapper.getOrderByOrderId(orderId);
     }
 }
---
+++ src/main/java/com/example/demo/mapper/master1/OrderMapper.java
@@ -0,0 +1,9 @@
+package com.example.demo.mapper.master1;
+
+import com.example.demo.dto.Order;
+
+public interface OrderMapper {
+
+    public Order getOrderByOrderId(String orderid);
+
+}
